99355fa811db8eb90b891489fd227ab873fdb06e
Subhro <amerpoth@gmail.com>
2023-04-15 12:03:42 +0530

    Database Connection setup

--- newsportalbackend/server.js
+++ newsportalbackend/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const dotenv=require('dotenv').config();
+const mongoose=require('mongoose');
 const app=express();
 
-const PORT=process.env.PORT|| 3030;
-app.listen(PORT,()=>{
-    console.log("Server running on port: "+PORT);
-})
+
+//connect database first the start the server
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=>{
+        const PORT=process.env.PORT|| 3030;
+        app.listen(PORT,()=>{
+            console.log("Database Connected\nServer running on port: "+PORT);
+        })
+    })
