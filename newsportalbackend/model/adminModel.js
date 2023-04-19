2e358069219402a76c27aab235f7deeecbfe8683
Subhro <amerpoth@gmail.com>
2023-04-19 11:19:19 +0530

    bcrypt error fixed
    use normal function in the place of fat arrow function

--- newsportalbackend/model/adminModel.js
+++ newsportalbackend/model/adminModel.js
@@ -25,16 +25,17 @@ const adminSchema=mongoose.Schema({
     }
 
 });
-adminSchema.pre("save",async(next)=>{
+adminSchema.pre("save",async function (next){
+    if(!this.isModified("password")){
+        return next();
+    }
     try {
          const salt=await bcrypt.genSalt(10);
-        const hashCode=await bcrypt.hash(this.password,salt);
-        console.log(hashCode);
+         this.password=await bcrypt.hash(this.password,salt);
     next();
     } catch (error) {
         console.error(error);
     }
-   
 })
 
 
