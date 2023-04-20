4e71d747b30749342e72ca4f6eec63af5f703e91
Subhro <amerpoth@gmail.com>
2023-04-20 11:55:50 +0530

    Admin Controll
    Admin login and update done

--- newsportalbackend/controller/adminController.js
+++ newsportalbackend/controller/adminController.js
@@ -1,44 +1,96 @@
-const Admin=require('../model/adminModel');
-const jwt = require('jsonwebtoken');
-const dotenv=require('dotenv').config();
+const Admin = require("../model/adminModel");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
+const bcrypt = require("bcryptjs");
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.MY_SECRET_KEY, {
-      expiresIn: "1h",
-    });
-  };
+  return jwt.sign({ id }, process.env.MY_SECRET_KEY, {
+    expiresIn: "1h",
+  });
+};
 
-const registerAdmin=async(req,res)=>{
-    try {
-        const {name,emailid,password,loginid}=req.body;
-        if(!name || !emailid || !password || !loginid){
-            res.status(400).json({msg:"Provide all fields"});
-            return;
-        }
-        const existAdmin=await Admin.findOne({emailid});
-        if(existAdmin){
-            res.status(400).json({msg:"You are already registered"});
-            return;
-        }
-        const newAdmin=await Admin.create(req.body);
-        const {id}=newAdmin;
-        const authToken=generateToken(id);
-        res.cookie("authToken", authToken, {
-            path: "/",
-            httpOnly: true,
-            expires: new Date(Date.now() + 1000 * 86400*2), // 2 day
-            sameSite: "none",
-            secure: false,
-          });
-          res.json({msg:"See Cookie"})
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({msg:"Some error occure while registering admin"});
+const registerAdmin = async (req, res) => {
+  try {
+    const { name, emailid, password, loginid } = req.body;
+    if (!name || !emailid || !password || !loginid) {
+      res.status(400).json({ msg: "Provide all fields" });
+      return;
     }
-}
+    const existAdmin = await Admin.findOne({ emailid });
+    if (existAdmin) {
+      res.status(400).json({ msg: "You are already registered" });
+      return;
+    }
+    const newAdmin = await Admin.create(req.body);
+    const { id } = newAdmin;
+    const authToken = generateToken(id);
+    res.cookie("authToken", authToken, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400 * 2), // 2 day
+      sameSite: "none",
+      secure: false,
+    });
+    res.json({ msg: "Admin registration successfull" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ msg: "Some error occure while registering admin" });
+  }
+};
 
+const adminLogin = async (req, res) => {
+  try {
+    const { password, loginid } = req.body;
+    if (!password || !loginid) {
+      res.status(400).json({ msg: "Provide all fields" });
+      return;
+    }
+    const existAdmin = await Admin.findOne({ loginid });
+    if (!existAdmin) {
+      res
+        .status(400)
+        .json({ msg: "You are not registered,Please Register yourself" });
+      return;
+    }
+    const passChek = await bcrypt.compare(password, existAdmin.password);
+    if (!passChek) {
+      res.status(400).json({ msg: "Invalid loginid or Password" });
+      return;
+    }
+    const { id } = existAdmin;
+    const authToken = generateToken(id);
+    res.cookie("authToken", authToken, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 28800), // 2 day
+      sameSite: "none",
+      secure: false,
+    });
+    res.status(200).json({ msg: "Admin Login successfull" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ msg: "Some error occure while login admin" });
+  }
+};
 
+const adminUpdate = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const findAdmin = await Admin.findOne({ _id });
+    if(!findAdmin){res.status(400).json({msg:"Admin not exist"});return;}
+    const updtedAdmin=await Admin.updateOne({_id},req.body);
+    if(!(updtedAdmin.modifiedCount ===1)){res.status(400).json({ msg: "Admin Update unsuccessfull" });}
+    res.status(200).json({status:updtedAdmin.modifiedCount,msg:"Admin Update Successfull"});
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ msg: "Some error occure while Updating admin" });
+  }
+};
 
+const clearAllCookies = (req, res) => {
+  res.clearCookie("authToken");
+  res.clearCookie("token");
+  res.send("Cookie clear");
+};
 
-
-module.exports={registerAdmin}
+module.exports = { registerAdmin, adminLogin, adminUpdate, clearAllCookies };
--- newsportalbackend/route/adminRoute.js
+++ newsportalbackend/route/adminRoute.js
@@ -1,9 +1,19 @@
 const express=require('express');
 const route=express.Router();
-const {registerAdmin}=require('../controller/adminController');
+const {registerAdmin,adminLogin,adminUpdate,clearAllCookies,}=require('../controller/adminController');
 
 //register new admin
 route.post('/register',registerAdmin);
+
+//admin login
+route.get('/login',adminLogin);
+
+//admin update
+route.put('/update/:id',adminUpdate);
+
+
+//clear all cookie (for dev perpouse only)
+route.delete('/clearcookie',clearAllCookies)
 
 
 
